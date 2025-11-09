1ee8955328a6dbc1c3dace7eea1cb1d427d44db6
MihaiAlexandru-Radu <mira3628@colorado.edu>
2025-11-09 14:35:44 -0700

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
       </Head>
 
       <main>
-        <h1 className='title'>Congratulations! You've set up your webapp!</h1>
+        <h1 className='title'>CSCI 1000 Group Project - Our Personal Pages</h1>
 
         <p className='description'>
           Get started by editing <code>src/pages/index.js</code> to be your
